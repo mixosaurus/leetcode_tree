1d5d4f9316c4d747b0321946f25bac95cd261570
snake <lcpdfk@163.com>
2020-08-10 17:45:22 +0800

    671. 二叉树中第二小的节点

--- Second_Minimum_Node_In_a_Binary_Tree/main.cpp
+++ Second_Minimum_Node_In_a_Binary_Tree/main.cpp
@@ -89,13 +89,7 @@ void traversePreorderNonRecursion(TreeNode* root) {
 TreeNode* createBinaryTreeManually() {
 	TreeNode* root = new TreeNode(2);
 	root->left = new TreeNode(2);
-	//root->left->left = new TreeNode(2);
-	//root->left->right = new TreeNode(3);
-	//root->left->right->left = new TreeNode(3);
-	//root->left->right->right = new TreeNode(6);
 	root->right = new TreeNode(5);
-	//root->right->left = new TreeNode(5);
-	//root->right->right = new TreeNode(7);
 	return root;
 }
 

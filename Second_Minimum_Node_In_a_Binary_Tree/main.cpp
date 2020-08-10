ebc6151914987b259c4acf79f9449eef7a672176
snake <lcpdfk@163.com>
2020-08-10 17:36:36 +0800

    :

---
+++ Second_Minimum_Node_In_a_Binary_Tree/main.cpp
@@ -0,0 +1,108 @@
+#include "solution.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+//根据层次遍历序列和中序遍历序列生成二叉树
+TreeNode* createBinaryTree(vector<int> level, vector<int> inorder) {
+	if (level.size() == 0 || inorder.size() == 0) {
+		return nullptr;
+	}
+	TreeNode* root = new TreeNode();
+	vector<int> leftInorder;
+	vector<int> rightInorder;
+	vector<int> leftLevel;
+	vector<int> rightLevel;
+	//将中序遍历序列根据层次遍历序列首元素分割为两部分，即左右子树的中序遍历序列
+	for (auto it = inorder.begin(); it != inorder.end(); it++) {
+		if (*it == *level.begin()) {
+			leftInorder=*(new vector<int>(inorder.begin(),it));
+			rightInorder =*(new vector<int>(it+1,inorder.end()));
+			break;
+		}
+	}
+	//根据左右子树的中序遍历序列，找出相应的层次遍历序列
+	for (auto it = level.begin()+1; it != level.end(); it++) {	
+		vector<int>::iterator iLeftLevel = find(leftInorder.begin(), leftInorder.end(), *it);
+		vector<int>::iterator iRightLevel = find(rightInorder.begin(), rightInorder.end(), *it);
+		if(iLeftLevel!= leftInorder.end()){
+			leftLevel.push_back(*iLeftLevel);
+		}
+		if (iRightLevel != rightInorder.end()) {
+			rightLevel.push_back(*iRightLevel);
+		} 
+	}
+	root->val = *level.begin();
+	root->left = createBinaryTree(leftLevel,leftInorder);
+	root->right = createBinaryTree(rightLevel,rightInorder);
+	return root;
+}
+
+//中序遍历，非递归
+void traverseInorderNonRecursion(TreeNode* root) {
+	if (root == nullptr) {
+		cout << "二叉树为空" << endl;
+		return;
+	}
+	stack<TreeNode*> stack;
+	TreeNode* p = root;
+	while (!stack.empty() || p != nullptr) {
+		while (p!=nullptr) {
+			stack.push(p);
+			p = p->left;
+		}
+		if (!stack.empty()) {
+			p = stack.top();
+			stack.pop();
+			cout << p->val << " ";
+			p = p->right;
+		}
+	}
+	cout << endl;
+}
+
+//前序遍历，非递归
+void traversePreorderNonRecursion(TreeNode* root) {
+	if (root == nullptr) {
+		cout << "二叉树为空" << endl;
+		return;
+	}
+	stack<TreeNode*> stack;
+	TreeNode* p = nullptr;
+	stack.push(root);
+	while (!stack.empty()) {
+		p = stack.top();
+		stack.pop();
+		cout << p->val << " ";
+		if (p->right != nullptr) {
+			stack.push(p->right);
+		}
+		if (p->left != nullptr) {
+			stack.push(p->left);
+		}
+	}
+}
+
+//手动建立二叉树，用于节点值允许重复的情况
+TreeNode* createBinaryTreeManually() {
+	TreeNode* root = new TreeNode(2);
+	root->left = new TreeNode(2);
+	//root->left->left = new TreeNode(2);
+	//root->left->right = new TreeNode(3);
+	//root->left->right->left = new TreeNode(3);
+	//root->left->right->right = new TreeNode(6);
+	root->right = new TreeNode(5);
+	//root->right->left = new TreeNode(5);
+	//root->right->right = new TreeNode(7);
+	return root;
+}
+
+//671. 二叉树中第二小的节点
+int main(){
+	TreeNode* tree = createBinaryTreeManually();
+	cout << (new Solution())->findSecondMinimumValue(tree) << endl;
+	system("pause");
+	return 0;
+}
---
+++ Second_Minimum_Node_In_a_Binary_Tree/solution.cpp
@@ -0,0 +1,42 @@
+#include "solution.h"
+#include <iostream>
+
+//将当前节点和其孩子节点共三个节点作为研究主体
+//在这三个节点中找出第二小节点即可解决问题
+//若当前节点值与其左孩子相等且与右孩子不相等，则当前节点的右孩子为第二小节点，另一种情况同理
+int Solution::findSecondMinimumValue(TreeNode* root) {
+    //null和叶子节点结束本次执行
+    //每个节点的孩子节点数只能为0或2
+    //因此只判断左右孩子之一是否存在即可确定是否为叶子节点
+    if (root == nullptr || root->left == nullptr) {
+        return -1;
+    }
+    int leftMin = findSecondMinimumValue(root->left);
+    int rightMin = findSecondMinimumValue(root->right);
+    //三节点相等
+    if (root->left->val == root->val && root->right->val == root->val) {
+        if (leftMin == -1) {
+            return rightMin;
+        } else if (rightMin == -1) {
+            return leftMin;
+        } else {
+            return min(leftMin, rightMin);
+        }
+    } else if (root->val==root->left->val) {
+        //当前节点的值与其左孩子相等
+        if (leftMin == -1) {
+            //右孩子较大，因此为第二小
+            return root->right->val;
+        } else {
+            return min(leftMin, root->right->val);
+        }
+    } else {
+        //当前节点值与其右孩子的值相等
+        if (rightMin == -1) {
+            //左孩子较大，因此为第二小
+            return root->left->val;
+        } else {
+            return min(rightMin, root->left->val);
+        }
+    }
+}
